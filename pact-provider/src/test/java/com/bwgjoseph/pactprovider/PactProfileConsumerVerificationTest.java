f80daf2a3f24a05d4d1142c254a67f208b2d24dd
bwgjoseph <josephgan@live.com.sg>
2022-06-03 15:25:53 +0800

    add pact provider junit test - verify consumer contract

---
+++ pact-provider/src/test/java/com/bwgjoseph/pactprovider/PactProfileConsumerVerificationTest.java
@@ -0,0 +1,54 @@
+package com.bwgjoseph.pactprovider;
+
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Provider("ProfileProvider")
+@PactFolder("pacts")
+public class PactProfileConsumerVerificationTest {
+    @MockBean
+    ProfileController profileController;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    /**
+     * Ensure the test will use the mock data rather than actual data
+     */
+    @State(value = "profiles 1 exists")
+    void profilesWithId1() {
+        when(profileController.getProfile(1))
+            .thenReturn(Optional.of(Profile.builder().id(1).name("Joseph").age(22).email("jose@gmail.com").dob(LocalDate.of(2000, 1, 1)).build()));
+    }
+
+    @State(value = "profiles exists")
+    void profiles() {}
+}
